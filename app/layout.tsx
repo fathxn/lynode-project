65b11d42fdc0f020f38a5bc6d53c75d2ffe6a982
User <user@Users-MacBook-Pro.local>
2025-07-22 00:07:13 +0700

    add metadata tag seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,50 @@
-'use client';
-
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { config } from '@/lib/wagmi';
-import { useState } from 'react';
 import { Roboto } from 'next/font/google';
+import Providers from './providers'; 
 
-const inter = Inter({ subsets: ['latin'] });
 const robotoMono = Roboto({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: "Lynode - Decentralized AI Compute Network",
+  description: "Rent GPU and CPU using blockchain securely and easily.",
+  icons: {
+    icon: '/favicon.png',
+  },
+  openGraph: {
+    title: "GPU Rent Platform | Web3",
+    description: "Rent GPU and CPU using blockchain securely and easily.",
+    url: "https://lynode-project.vercel.app/",
+    siteName: "GPU Rent",
+    images: [
+      {
+        url: "/logo-lynode.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "GPU Rent Platform | Web3",
+    description: "Rent GPU and CPU using blockchain securely and easily.",
+    images: ["/logo-lynode.png"],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html lang="en">
       <body className={robotoMono.className}>
-        <QueryClientProvider client={queryClient}>
-          <WagmiProvider config={config}>
-            {children}
-          </WagmiProvider>
-        </QueryClientProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { config } from '@/lib/wagmi'; // kamu sesuaikan jika beda lokasi
+import { useState } from 'react';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>{children}</WagmiProvider>
+    </QueryClientProvider>
+  );
+}
